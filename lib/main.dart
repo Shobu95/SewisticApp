1619a506574e19ce392e59e279551331cda0ef9f
samdid <sumit.21910448@viit.ac.in>
2021-10-02 15:29:03 +0530

    Animations added to splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -17,3 +17,51 @@ class SewisticApp extends StatelessWidget {
     );
   }
 }
+// import 'package:flutter/material.dart';
+// import 'package:animated_splash/animated_splash.dart';
+//
+// void main() {
+//   Function duringSplash = () {
+//     print('Something background process');
+//     int a = 123 + 23;
+//     print(a);
+//
+//     if (a > 100)
+//       return 1;
+//     else
+//       return 2;
+//   };
+//
+//   Map<int, Widget> op = {1: Home(), 2: Home()};
+//
+//   runApp(MaterialApp(
+//     home: AnimatedSplash(
+//       imagePath: 'assets/images/sewistic-logo-web.png',
+//       home: Home(),
+//       customFunction: duringSplash,
+//       duration: 3500,
+//       type: AnimatedSplashType.BackgroundProcess,
+//       outputAndHome: op,
+//     ),
+//   ));
+// }
+//
+// class Home extends StatefulWidget {
+//   @override
+//   _HomeState createState() => _HomeState();
+// }
+//
+// class _HomeState extends State<Home> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(
+//           title: Text('GeeksForGeeks'),
+//           backgroundColor: Colors.green,
+//         ),
+//         body: Center(
+//             child: Text('My Home Page',
+//                 style: TextStyle(color: Colors.black,
+//                     fontSize: 20.0))));
+//   }
+// }
--- lib/screens/intro/splash_screen.dart
+++ lib/screens/intro/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:sewistic_app/screens/intro/intro_slider/intro_slider.dart';
+import 'package:shimmer/shimmer.dart';
 
 class SplashScreen extends StatelessWidget {
   @override
@@ -9,9 +10,31 @@ class SplashScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
       body: Center(
-        child: Image(
-          image: AssetImage('assets/images/sewistic-logo-web.png'),
-          color: Colors.white,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Shimmer.fromColors(
+              period: Duration(milliseconds: 2500),
+              baseColor: Colors.black,
+              highlightColor: Colors.white.withOpacity(0.7),
+              child: Container(
+                padding: EdgeInsets.all(5.0),
+                child: Container(
+                  height: 110,
+                  child: Image.asset('assets/images/sewistic-logo-web.png',
+                  fit: BoxFit.fill,)
+                ),
+              ),
+            ),
+//                Text(
+//                  "LINK",
+//                  style: TextStyle(
+//                      letterSpacing: 3,
+//                      fontSize: 50.0,
+//                      fontWeight: FontWeight.bold,
+//                  ),
+//                ),
+          ],
         ),
       ),
     );
