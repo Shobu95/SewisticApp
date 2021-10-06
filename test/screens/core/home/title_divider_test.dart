564fef192950b6633257237bfe0027c8c55972fc
jushutch <jushutch@umich.edu>
2021-10-05 20:09:49 -0400

    Remove unused import

--- test/screens/core/home/title_divider_test.dart
+++ test/screens/core/home/title_divider_test.dart
@@ -1,4 +1,3 @@
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:sewistic_app/screens/core/home/widgets/title_divider.dart';
